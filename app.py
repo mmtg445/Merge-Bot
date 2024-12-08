447b115db97e14e3ba9374b5ffdb327b5bdae9ae
mmtg445 <mmtg445@gmail.com>
2024-12-08 17:13:06 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route("/")
 def hello_world():
     return "CodeXBots"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
