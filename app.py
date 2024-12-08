ba744f43c3094e6c9b09821424ddd1f58ee60641
mmtg445 <mmtg445@gmail.com>
2024-12-09 00:12:38 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route("/")
-def hello_world():
-    return "CodeXBots"
+def home():
+    return "Hello, Heroku!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Heroku ডাইনামিক পোর্ট থেকে মান নেয়
+    app.run(host="0.0.0.0", port=port)
