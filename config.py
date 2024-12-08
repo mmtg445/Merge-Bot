1dc702d2cec8c7ee194a7079c5e48e7f0af1036a
mmtg445 <mmtg445@gmail.com>
2024-12-08 16:42:22 +0600

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7860716079:AAFCAo1JQsbBqMhBjx5kJ93D6Ng6Mo1o0go")
     TELEGRAM_API = os.environ.get("TELEGRAM_API", "8564fab8db759bb04b1907bd12ed98ef")
     OWNER = os.environ.get("OWNER", "8102446291")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", ""RahatVx)
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "RahatVx")
     PASSWORD = os.environ.get("PASSWORD", "rindex")
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://rohanahamed75:gt4RXJZ1mUtOh4Xv@mmtg.0ong5.mongodb.net/?retryWrites=true&w=majority&appName=mmtg")
     LOGCHANNEL = os.environ.get("LOGCHANNEL" ,"-1002289024376")  # Add channel id as -100 + Actual ID
