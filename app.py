8a07c2fbcfbe312af78fb29b891f0f1f8c0ada15
mmtg445 <mmtg445@gmail.com>
2024-12-08 23:47:02 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "CodeXBots"
+def home():
+    return "Hello, Heroku!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
